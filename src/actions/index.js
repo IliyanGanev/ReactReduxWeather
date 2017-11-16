d84775a87d1db3f5b1a1fdebaf4c768c6947ed61
Iliyan Ganev <ganeviliyan@gmail.com>
2017-11-15 21:57:16 -0800

    Add Axios and insert API KEY

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+const API_KEY = '8450a715709795fde2f483a204e2a501';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+
+	return {
+		type: FETCH_WEATHER
+	};
+}
