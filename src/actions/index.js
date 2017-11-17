be951ece05927ff925ef9f86cf1e58dcc296df9d
Iliyan Ganev <ganeviliyan@gmail.com>
2017-11-16 21:21:19 -0800

    Get temp diagram to show correctly

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const API_KEY = '8450a715709795fde2f483a204e2a501';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
@@ -5,8 +7,11 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
 	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
 
 	return {
-		type: FETCH_WEATHER
+		type: FETCH_WEATHER,
+		payload: request
 	};
 }
